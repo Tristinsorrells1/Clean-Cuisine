eb8c79ded74a5dfca8c9181fbdf59f78b88ed846
Tristin Sorrells <Tristinsorrells1@gmail.com>
2023-03-01 10:24:43 -0600

    Add input error handling for the form in the Home component

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,7 +5,25 @@ const Home = () => {
 
   const [zipcode, setZipcode] = useState("")
   const [name, setName] = useState("")
+  const [invalidZip, setInvalidZip] = useState("")
+  const [emptyInputs, setEmptyInputs] = useState("")
 
+  let checkInputs = (event) => {
+    event.preventDefault();
+    let zipCheck = /^\d{5}$/.test(zipcode)
+    if (zipcode && !zipCheck) {
+      setInvalidZip(true)
+    }
+    else if (!zipcode || zipCheck) {
+      setInvalidZip(false)
+    }
+    if (zipcode.trim() || name.trim()) {
+      setEmptyInputs(false)
+    }
+    else {
+      setEmptyInputs(true);
+    }
+  }
 
   return (
     <section className="home-section">
@@ -16,15 +34,29 @@ const Home = () => {
           placeholder="Enter Zipcode"
           name="zipcode"
           value={zipcode}
+          onChange={(event) => setZipcode(event.target.value)}
         />
         <input
           type="text"
           placeholder="Restaurant Name"
           name="name"
           value={name}
+          onChange={(event) => setName(event.target.value)}
         />
-        <button>Search</button>
+        <button onClick={(event) => checkInputs(event)}>Search</button>
       </form>
+      {invalidZip && (
+        <div className="error-container">
+          <p className="form-error">Error - Invalid Zipcode</p>
+          <p>Please enter a valid zipcode, or search by name instead.</p>
+        </div>
+      )}
+      {emptyInputs && !invalidZip && (
+        <div className="error-container">
+          <p className="form-error">Error - Missing Inputs</p>
+          <p>Please enter a zipcode, and/or a restaurant's name</p>
+        </div>
+      )}
     </section>
   );
 };
