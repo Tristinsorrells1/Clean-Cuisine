8cec871c04369a1efa3a8d9900d0e66a5091a31a
Tristin Sorrells <Tristinsorrells1@gmail.com>
2023-03-01 19:55:06 -0600

    Add a back button in Details

--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -3,6 +3,7 @@ import "./Details.css";
 import { useParams } from "react-router-dom";
 import { getRestaurant } from "../../APICalls";
 import { cleanData, removeDuplicates } from "../../util";
+import { NavLink } from "react-router-dom";
 
 const Details = (  ) => {
   const params = useParams()
@@ -18,41 +19,46 @@ useEffect(() => {
 }, []);
 
   return (
-    <section className="details-section">
-      <div className="address-and-iframe-container">
-        <div className="name-and-address-container">
-          <h2>{restaurant.name}</h2>
-          <p className="street">{restaurant.address}</p>
-          <p className="city">{`${restaurant.city}, IL`}</p>
-          <p className="zip">{restaurant.zip}</p>
+    <>
+    <NavLink to={"/"}>
+      <button>Back to Results</button>
+    </NavLink>
+      <section className="details-section">
+        <div className="address-and-iframe-container">
+          <div className="name-and-address-container">
+            <h2>{restaurant.name}</h2>
+            <p className="street">{restaurant.address}</p>
+            <p className="city">{`${restaurant.city}, IL`}</p>
+            <p className="zip">{restaurant.zip}</p>
+          </div>
+          <div className="iframe-placeholder"></div>
         </div>
-        <div className="iframe-placeholder"></div>
-      </div>
-      <div>
-        <div className="inspection-results-header">Inspection Results</div>
-        <div className="inspection-details">
-          <p className="date">{`Date of Inspection: ${restaurant.date}`}</p>
-          <p className="risk">{`Risk: ${restaurant.risk}`}</p>
-          <p className="result">{`Result: ${restaurant.result}`}</p>
+        <div>
+          <div className="inspection-results-header">Inspection Results</div>
+          <div className="inspection-details">
+            <p className="date">{`Date of Inspection: ${restaurant.date}`}</p>
+            <p className="risk">{`Risk: ${restaurant.risk}`}</p>
+            <p className="result">{`Result: ${restaurant.result}`}</p>
+          </div>
         </div>
-      </div>
 
-      <div>
-        <div className="violation-header">Violations</div>
-        <div className="violation-details">
-          <p className="date">{restaurant.violations}</p>
+        <div>
+          <div className="violation-header">Violations</div>
+          <div className="violation-details">
+            <p className="date">{restaurant.violations}</p>
+          </div>
         </div>
-      </div>
 
-      <div>
-        <div className="yelp-header">Yelp Reviews</div>
-        <div className="yelp-container">
-          <div className="yelp-placeholder"></div>
-          <div className="yelp-placeholder"></div>
-          <div className="yelp-placeholder"></div>
+        <div>
+          <div className="yelp-header">Yelp Reviews</div>
+          <div className="yelp-container">
+            <div className="yelp-placeholder"></div>
+            <div className="yelp-placeholder"></div>
+            <div className="yelp-placeholder"></div>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
