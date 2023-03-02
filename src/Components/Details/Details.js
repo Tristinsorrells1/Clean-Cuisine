c081cf981feead3f6f539e196e73719b68bc9788
Tristin Sorrells <Tristinsorrells1@gmail.com>
2023-03-01 19:50:52 -0600

    Add basic styling to details page

--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -18,10 +18,39 @@ useEffect(() => {
 }, []);
 
   return (
-    <section>
+    <section className="details-section">
+      <div className="address-and-iframe-container">
+        <div className="name-and-address-container">
+          <h2>{restaurant.name}</h2>
+          <p className="street">{restaurant.address}</p>
+          <p className="city">{`${restaurant.city}, IL`}</p>
+          <p className="zip">{restaurant.zip}</p>
+        </div>
+        <div className="iframe-placeholder"></div>
+      </div>
       <div>
-        <h2>{restaurant.name}</h2>
-        <h2>tesst</h2>
+        <div className="inspection-results-header">Inspection Results</div>
+        <div className="inspection-details">
+          <p className="date">{`Date of Inspection: ${restaurant.date}`}</p>
+          <p className="risk">{`Risk: ${restaurant.risk}`}</p>
+          <p className="result">{`Result: ${restaurant.result}`}</p>
+        </div>
+      </div>
+
+      <div>
+        <div className="violation-header">Violations</div>
+        <div className="violation-details">
+          <p className="date">{restaurant.violations}</p>
+        </div>
+      </div>
+
+      <div>
+        <div className="yelp-header">Yelp Reviews</div>
+        <div className="yelp-container">
+          <div className="yelp-placeholder"></div>
+          <div className="yelp-placeholder"></div>
+          <div className="yelp-placeholder"></div>
+        </div>
       </div>
     </section>
   );
