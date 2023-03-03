a41830c9361d14970a728b36fd396d99fb9b90a7
Tristin Sorrells <Tristinsorrells1@gmail.com>
2023-03-03 10:54:49 -0600

    Add basic styling to the result cards

--- src/Components/ResultCard/ResultCard.js
+++ src/Components/ResultCard/ResultCard.js
@@ -7,14 +7,24 @@ import { NavLink } from "react-router-dom";
 const ResultCard = ({  name, risk, address, city, zip, date, result, violations, license }) => {
   return (
     <>
-    <NavLink to={`/${license}`} className="NavLink">
-      <div className="result-card">
-        <p className="card-name">{name}</p>
-        <p className="card-result">{result}</p>
-      </div>
-    </NavLink>
+      <NavLink to={`/${license}`} className="NavLink">
+        <div className="result-card">
+          <div className="name-container">
+            <p className="card-name">{name}</p>
+          </div>
+          <div className="result-container">
+            <p className="card-result">{result}</p>
+          </div>
+          {result.includes("Pass") && (
+            <img
+              className="tired"
+              src="https://www.iconsdb.com/icons/preview/green/check-mark-2-xxl.png"
+            ></img>
+          )}
+        </div>
+      </NavLink>
     </>
-  )
+  );
 };
 
 export default ResultCard
